d1f8c7d7ab6907a8a8c6fc358a71095b35ce2d71
Paulo Cesar Rodriguez Contreras <rodriguezpaulo2373@gmail.com>
2021-10-25 12:16:48 -0500

    Finishing the try

--- README.md
+++ README.md
@@ -2,8 +2,39 @@
 
 WEB SERVICE CHALLENGE
 
+Developed functions can be found in the following path "codetest/polls/views.py"
+
+Questions:
 1. What would you do differently if you had more time?
+Learn a little more about IP address tracking and server management. I would try to show the related program information in a better way.
+
 2. What is the runtime complexity of each function?
+I would have to evaluate it at a higher level.
+
 3. How does your code work?
+The program should call a server or connect to a service to receive the requests and work with them. On the user side it should have options to perform the top100() and clear() actions.
+
 4. What other approaches did you decide not to pursue?
+The concept of Django applications, handling requests and servers through sockets or semaphores.
+
 5. How would you test this?
+By connecting to a server that allows me to work with the volume of inputs described in this challenge. Even getting the data through a REST API service.
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+Translated with www.DeepL.com/Translator (free version)
--- codetest/polls/views.py
+++ codetest/polls/views.py
@@ -1,4 +1,44 @@
 from django.http import HttpResponse
+from requests import get
+import socket 
+import re
+from collections import Counter
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):    
+    print("This is a codetest")
+
+"""This function will be called by the web service every time it handles a request
+
+    Parameters
+    ----------
+    ip_address : str
+        string containing an IP address
+"""
+def request_handled(ip_address):         
+    IPAddr = socket.gethostbyname(ip_address)    
+    print("The Computer IP Address is:" + IPAddr) 
+
+""" This function returns the top 100 IP addresses by request count, with the highest traffic IP address first
+"""
+def top100():    
+    cnt = Counter()
+    ipre = re.compile(r'^(?P<ip>(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])) - -')
+    with open(infilename) as infile:
+        for line in infile:
+            m = ipre.match(line)
+            if m is not None:
+                ip = m.groupdict()['ip']
+                cnt[ip] += 1
+
+""" It's used to forget about all IP addresses and tallies.
+"""
+def clear():
+# Making a DELETE request
+r = requests.delete('list_of_ips', data ={'key':'value'})
+  
+# check status code for response received
+# success code - 200
+print(r)
+  
+# print content of request
+print(r.json())
